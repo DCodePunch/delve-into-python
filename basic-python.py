614b9dcc275a7bc131a1a1558cd45e4d3ce00e8f
Akshara <121200900+DCodePunch@users.noreply.github.com>
2024-09-18 10:49:15 +0000

    modified:   basic-python.py

--- basic-python.py
+++ basic-python.py
@@ -4,8 +4,10 @@ TP = 50
 FN = 40
 FP = 60
 
+#print(TN)
+
 Sensitivity = TP / (TP + FN)
 Specificity = TN / (TN + FP)
 
 BalancedAccuracy = (Sensitivity + Specificity)/2
-BalancedAccuracy
+print(BalancedAccuracy)
